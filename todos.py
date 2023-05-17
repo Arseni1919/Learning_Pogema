5bb62796711c02892d1f758c370d10a7b5ba00ae
perchik_air <1919ars@gmail.com>
2023-05-17 19:24:14 +0300

    more

--- todos.py
+++ todos.py
@@ -1,12 +1,13 @@
 """
 DONE:
+- usage instructions in README.md
 
 TODO:
-- usage instructions in README.md
 - build metrics
 - implement versions of SDS or MGDS or both
 - implement learning (Q-learning, for example)
 - compare to their A* - big experiments
+- save results
 - plot graphs
 - write a report + presentation
 - submit to professor
